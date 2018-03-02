1b5159788421d6a5030dcb873552c22b2110b641
Michael Arnold <minimearnold@gmail.com>
2018-03-02 15:15:37 -0800

    Practical Components 1, 2, & part of 3
    Still implementing tabs

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-
-
 Vue.component('task-list', {
 
   // Have to wrap <task> in <div> beacuse of v-for ---> Render function should return a single root node.
@@ -26,16 +24,117 @@ Vue.component('task-list', {
 
 });
 
-
-
 Vue.component('task', {
 
   template: '<li><slot></slot></li>'
 
 });
 
+Vue.component('message-component', {
+
+    // Have to be specific as to what is being passed through (ie title, body)
+    props: ['title', 'body'],
+
+    data() {
+
+      return {
+
+        isVisisble: true
+
+      };
+    },
+
+    template:`
+
+      <article class="message" v-show="isVisisble">
+
+        <div class="message-header">
+
+          {{ title }}
+
+          <button class="delete" aria-label="delete" @click="isVisisble = false"></button>
+
+        </div>
+
+        <div class="message-body">
+
+            {{ body }}
+
+        </div>
+
+      </article>
+
+    `,
+
+});
+
+
+
+Vue.component('modal-component', {
+
+  template: `
+      <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+          <div class="box">
+            <slot></slot>
+          </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close-modal')"></button>
+      </div>
+
+  `
+
+});
+
+Vue.component('tabs', {
+
+  template: `
+    <div>
+      <div class="tabs">
+        <ul>
+          <li v-for="tab in tabs" :class="{'is-active': tab.selected }">
+            <a href='#'> {{ tab.name }} </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="tabs-details">
+        <slot></slot>
+      </div>
+    </div>
+  `,
+
+  data() {
+    return { tabs: [] };
+  },
+
+  created() {
+    this.tabs = this.$children;
+  }
+
+});
+
+
+Vue.component('tab', {
+
+  props: {
+    name: {required: true},
+    selected: {default: false}
+  },
+
+  template: `
+    <div><slot></slot></div>
+  `
+
+});
+
 
 new Vue({
-  el: '#root'
+  el: '#root',
+
+  data: {
+    showModal: false
+  }
 
 });
