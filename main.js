799e7fde1ab7cfe5cc8b4e3542c425c469c2a349
Michael Arnold <minimearnold@gmail.com>
2018-03-05 11:48:25 -0800

    Single-Use Components and inline templates

--- main.js
+++ main.js
@@ -1,33 +1,9 @@
 
-Vue.component('modal', {
+Vue.component('progress-view', {
+  data() {
+    return { completionRate: 50 };
+  }
 
-  template:
-    `<div class="modal is-active">
-
-      <div class="modal-background"></div>
-
-      <div class="modal-card">
-
-        <header class="modal-card-head">
-          <p class="modal-card-title"><slot name="header"></slot></p>
-          <button class="delete" aria-label="close"></button>
-        </header>
-
-        <section class="modal-card-body">
-          <slot>
-            Default Content Here.
-          </slot>
-        </section>
-
-        <footer class="modal-card-foot">
-          <slot name="footer">
-            <button class="button is-success">Okay</button>
-          </slot>
-        </footer>
-
-      </div>
-
-    </div>`
 
 });
 
